4680369ed18e7da3ea423ae87b1c2bfb0794bfda
Mohamedlee27 <leonk8070@gmail.com>
2022-04-10 21:41:57 +0300

    created a new component

--- IP/src/app/app.module.ts
+++ IP/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    FormComponent
+    FormComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
