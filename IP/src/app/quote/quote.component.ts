f2d34bfdc1b91beb3f0c48f436766965f4bcb7b1
Mohamedlee27 <leonk8070@gmail.com>
2022-04-10 21:38:08 +0300

    added import and export classes

--- IP/src/app/quote/quote.component.ts
+++ IP/src/app/quote/quote.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes=[
+    new Quote('Lee','Michael','The greatest glory in living lies not in never falling, but in rising every time we fall. ', new Date)
+  ]
 
   constructor() { }
 
